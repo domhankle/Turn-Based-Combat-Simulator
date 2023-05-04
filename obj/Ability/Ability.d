b0cfd9b3a2d08ec6be54993398f2d3903a917cb3
domhankle <domhankle03@gmail.com>
2023-05-04 14:07:38 -0400

    5.4.2023.2
    Make clean call for previous commit

--- obj/Ability/Ability.d
+++
@@ -1,3 +0,0 @@
-obj/Ability/Ability.o: src/Ability/Ability.cpp \
- src/Ability/../../include/Ability/Ability.h
-src/Ability/../../include/Ability/Ability.h:
--- obj/Entity/Character.d
+++
@@ -1,7 +0,0 @@
-obj/Entity/Character.o: src/Entity/Character.cpp \
- src/Entity/../../include/Entity/Character.h \
- src/Entity/../../include/Entity/Entity.h \
- src/Entity/../../include/Entity/../Ability/Ability.h
-src/Entity/../../include/Entity/Character.h:
-src/Entity/../../include/Entity/Entity.h:
-src/Entity/../../include/Entity/../Ability/Ability.h:
