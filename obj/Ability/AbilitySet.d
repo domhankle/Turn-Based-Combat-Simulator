f20e0725d04740a15da3fe5616b2ce2fa1825493
domhankle <domhankle03@gmail.com>
2023-05-19 19:13:45 -0400

    5.19.2023.2
    Previous commit with make clean call

--- obj/Ability/AbilitySet.d
+++
@@ -1,9 +0,0 @@
-obj/Ability/AbilitySet.o: src/Ability/AbilitySet.cpp \
- src/Ability/../../include/Ability/AbilitySet.h \
- src/Ability/../../include/Ability/Ability.h \
- src/Ability/../../include/Ability/OffensiveAbility.h \
- src/Ability/../../include/Ability/DefensiveAbility.h
-src/Ability/../../include/Ability/AbilitySet.h:
-src/Ability/../../include/Ability/Ability.h:
-src/Ability/../../include/Ability/OffensiveAbility.h:
-src/Ability/../../include/Ability/DefensiveAbility.h:
